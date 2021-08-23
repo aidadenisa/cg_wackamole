332bb8d2641b05d0164f03a9b5af566449920b25
aidadenisa <aida.denisa@gmail.com>
2021-08-23 18:09:24 +0200

    added express node app and a static folder with all js files

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+var express = require("express");
+var app = express();
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+
+// var shaders = require(".publ/utils.js");
+// var shaders = require("./coords.js");
+// var shaders = require("./shaders.js");
+// var shaders = require("./script.js");
+
+ app.use(express.static('public'))
+
+//define the route for "/"
+app.get("/", function (request, response){
+ //show this file when the "/" is requested
+ response.sendFile(__dirname+"/public/index.html");
+});
+
+ //start the server
+app.listen(8080);
+
 console.log("RUNNING! listening on 8080")
