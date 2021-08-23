cda402e5f862dfc932ed4972d8b19531587db60a
aidadenisa <aida.denisa@gmail.com>
2021-08-23 13:01:05 +0200

    structured the code with drawscene and animate functions

--- script.js
+++ script.js
@@ -1,4 +1,253 @@
 
+var canvas;
+var gl = null,
+  program = null;
+
+var projectionMatrix,
+  perspectiveMatrix,
+  viewMatrix,
+  worldMatrix, vao, matrixLocation;
+var lastUpdateTime = (new Date).getTime();
+//Camera parameters
+var cx = 0.5;
+var cy = 0.0;
+var cz = 1.0;
+var elevation = 0.0;
+var angle = -30.0;
+
+var delta = 0.1;
+var flag = 0;
+
+//Cube parameters
+var cubeTx = 0.0;
+var cubeTy = 0.0;
+var cubeTz = -1.0;
+var cubeRx = 0.0;
+var cubeRy = 0.0;
+var cubeRz = 0.0;
+var cubeS = 0.5;
+
+
+function main() {
+
+  // Get the canvas and grab a WebGL context
+  canvas = document.getElementById("mycanvas");
+  gl = canvas.getContext("webgl2");
+  if (!gl) {
+    alert("GL context not opened");
+    return;
+  }
+
+  // autoResizeCanvas(canvas);
+  utils.resizeCanvasToDisplaySize(gl.canvas);
+
+  var aspectRatio = gl.canvas.width/gl.canvas.height;
+  //Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  //Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.enable(gl.CULL_FACE);
+  gl.enable(gl.DEPTH_TEST);
+
+
+  // you have to declare them here because you use them to send data to GPU
+  //x,y for each vertex with aspect ratio correction
+  // var positions = [
+  //       0.2, 0.1,
+  //       0.5, 0.9,
+  //       0.8, 0.7
+  // ];
+
+  // var colours = [
+  //   0.0, 1.0, 0.0,
+  //   0.0, 0.0, 1.0,
+  //       1.0, 0.0, 0.0,
+
+  // ];
+
+  //   //******Initialisation of color uniforms variables******/
+  // var colour = [252.0/255.0, 186.0/255.0, 3.0/255.0];
+  // var colour_choice = 0.0;
+  // var second_colour = [29.0/255.0, 219.0/255.0];
+
+  //positions for indexing: 
+  //x,y for each vertex
+  // var positionsInd = [
+  //       0.0, 0.0,
+  //       0.5, 0.9,
+  //       0.9, 0.0,
+  //       0.5, -0.9,
+  //       -0.5,-0.9,
+  //       -0.9, 0.0,
+  //       -0.5, 0.9,
+  // ];
+
+
+  // create GLSL shaders, upload the GLSL source, compile the shaders and link them
+  var vertexShader = utils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = utils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  program = utils.createProgram(gl, vertexShader, fragmentShader);
+
+  // look up where the vertex data needs to go.
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");  
+  var colorAttributeLocation = gl.getAttribLocation(program, "a_color");  
+  var matrixLocation = gl.getUniformLocation(program, "matrix");
+
+  // we create the perspective matrix only once because the perspective won't change
+  perspectiveMatrix = utils.MakePerspective(90, gl.canvas.width / gl.canvas.height, 0.1, 100.0);
+
+
+
+  // Create a vertex array object (attribute state) - for drawing the same object multiple times, like in multiple frames
+  vao = gl.createVertexArray();
+  // and make it the one we're currently working with
+  gl.bindVertexArray(vao);
+    
+  // Create a buffer and put three 2d clip space points in it
+  positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  // 3 components per iteration
+  // don't normalize the data
+  // 0 = move forward stride * sizeof(type) each iteration to get the next position
+  // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+  colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(colorAttributeLocation);
+  //params similar to top
+  gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+  indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW); 
+
+  // // Tell it to use our program (pair of shaders)
+  // gl.useProgram(program);
+
+
+  //    //******Retrieving the uniforms locations******/
+  // var colourLocation = gl.getUniformLocation(program, "u_colour");
+  // var colourChoiceLocation = gl.getUniformLocation(program, "colour_choice");
+  // var secondColourLocation = gl.getUniformLocation(program, "u_second_colour");
+
+
+  // //******Passing all the uniforms******/
+  // gl.uniform3fv(colourLocation, colour);
+  // gl.uniform1f(colourChoiceLocation, colour_choice);
+  // gl.uniform2fv(secondColourLocation, second_colour);
+
+
+  // //Draw call triangle
+  // var primitiveType = gl.TRIANGLES;
+  // var offset = 0;
+  // var count = 3;
+  // gl.drawArrays(primitiveType, offset, count);
+
+
+  //   ////////////// INDEX BUFFER
+  // //Create the buffer that will hold the indices and send the data
+  // var indices = [0,1,2,0,2,3,0,3,4,0,4,5,0,5,6,0,6,1];
+  // var indexBuffer = gl.createBuffer();
+  // //Here the buffer must be gl.ELEMENT_ARRAY_BUFFER to specify it contains indices
+  // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+
+  // //draw cube in perspective
+
+  // //Perspective matrix - instead of world matrix
+  // //params: FoVy (field of view, angle at the top of the frustum, aspect ratio, near plane distance, far plane distance
+  // var perspectiveMatrix = utils.MakePerspective(90, aspectRatio, 0.1, 100.0);
+  // console.log(perspectiveMatrix);
+
+  // //View Matrix - sets position of the camera and moves the model in camera space (eye coords)
+  // //params: camera position x, y, z, elevation (on x axis), angle (on y axis)
+  // var viewMatrix = utils.MakeView(0.5, 0.0, 1.0, 0.0, -30.0);
+  // console.log(viewMatrix);
+
+  // //Projection matrix - how the scene is perceived by the camera.
+  // var projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, viewMatrix);
+  
+  // // Set a the color as uniform. Pay attention! this line must be after "useProgram" otherwise
+  // //webgl is not able to find the colorLocation, and then to set its value 
+  // gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix)); 
+
+  // //make the index buffer the active one
+  // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+  // //drawElements uses the indices to draw the primitives
+  // gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0 );
+  //Indexed array buffer
+
+
+  drawScene();  
+
+  function animate() {
+    var currentTime = (new Date).getTime();
+    if (lastUpdateTime) {
+      var deltaC = (30 * (currentTime - lastUpdateTime)) / 1000.0;
+      cubeRx += deltaC;
+      cubeRy -= deltaC;
+      cubeRz += deltaC;
+
+      if (flag == 0) cubeS += deltaC / 100;
+      else cubeS -= deltaC / 100;
+
+      if (cubeS >= 1.5) flag = 1;
+      else if (cubeS <= 0.5) flag = 0;
+
+    }
+
+    //create world matrix for animated cube. needs to be done once per frame
+    worldMatrix = utils.MakeWorld(cubeTx, cubeTy, cubeTz,
+      cubeRx, cubeRy, cubeRz,
+      cubeS);
+    lastUpdateTime = currentTime;
+  }
+
+function drawScene() {
+    animate();
+
+    //repeat clearing the view and setting up the canvas, as well as using the program
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.useProgram(program);
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao);
+
+    var viewMatrix = utils.MakeView(cx, cy, cz, elevation, angle);
+
+    // perspective * view * world (we have to multiply them in the opposite order)
+    var projectionMatrix = utils.multiplyMatrices(viewMatrix, worldMatrix);
+    projectionMatrix = utils.multiplyMatrices(perspectiveMatrix, projectionMatrix);
+
+    gl.uniformMatrix4fv(matrixLocation, gl.FALSE, utils.transposeMatrix(projectionMatrix));
+
+    //bind buffer for drawing cube using indices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    // draw cube
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    //The requestAnimationFrame function tells the browser to call the specified function before the next repaint
+    //so that we can update the rendered image with the latest changes
+    window.requestAnimationFrame(drawScene);
+  }
+
+}
+
+
+
 //Utility function to compile the shaders
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -43,139 +292,6 @@ function autoResizeCanvas(canvas) {
   }
 
 
-
-function main() {
-
-  // Get the canvas and grab a WebGL context
-  var canvas = document.getElementById("mycanvas");
-  var gl = canvas.getContext("webgl2");
-  if (!gl) {
-    alert("GL context not opened");
-    return;
-  }
-
-  autoResizeCanvas(canvas);
-
-  // you have to declare them here because you use them to send data to GPU
-  //x,y for each vertex with aspect ratio correction
-  var positions = [
-        0.2, 0.1,
-        0.5, 0.9,
-        0.8, 0.7
-  ];
-
-  var colours = [
-    0.0, 1.0, 0.0,
-    0.0, 0.0, 1.0,
-        1.0, 0.0, 0.0,
-
-  ];
-
-  //positions for indexing: 
-  //x,y for each vertex
-  var positionsInd = [
-        0.0, 0.0,
-        0.5, 0.9,
-        0.9, 0.0,
-        0.5, -0.9,
-        -0.5,-0.9,
-        -0.9, 0.0,
-        -0.5, 0.9,
-  ];
-
-  ///////// SHADERS
-  
-  // create GLSL shaders, upload the GLSL source, compile the shaders
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
-  // Link the two shaders into a program
-  var program = createProgram(gl, vertexShader, fragmentShader);
-
-  var aspectRatio = gl.canvas.width/gl.canvas.height;
-
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer();
-
-  // Bind it to ARRAY_BUFFER because it's going to contain attribute data
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-
-  //////////// POSITION BUFFER
-
-  //Send data in positions to the currently-bound buffer in the gpu
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW); 
-  //Send data in positions to the currently-bound buffer in the gpu
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionsInd), gl.STATIC_DRAW);    
-  
-  //Look up where the vertex data needs to go.
-  //Assume vec4 a_position attribute in GLSL representing the position of the vertices
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-  //Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2;          // 2 components per iteration
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward stride * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, gl.FLOAT, normalize, stride, offset);
-
-
-  /////////// COLOR BUFFER
-
-    //Create a buffer to hold the rgb colours
-  var colourBuffer = gl.createBuffer();
-  // Bind it to ARRAY_BUFFER because it's going to contain attribute data
-  gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer);
-  //Send data in colours to the currently-bound buffer in the gpu
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colours), gl.STATIC_DRAW);
-  //Look up where the colour data needs to go
-  var colourAttributeLocation = gl.getAttribLocation(program, "a_colour");
-  //Enable the attribute
-  gl.enableVertexAttribArray(colourAttributeLocation);
-  //We are passing 3 values (rgb) at a time
-  size = 3;          
-  normalize = false; 
-  stride = 0;        
-  offset = 0;        
-  gl.vertexAttribPointer(colourAttributeLocation, size, gl.FLOAT, normalize, stride, offset);
-
-
-  //Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  //Clear the canvas
-  gl.clearColor(1, 1, 1, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  //Tell it to use our program 
-  gl.useProgram(program);
-
-
-
-  //Draw call triangle
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 3;
-  gl.drawArrays(primitiveType, offset, count);
-
-
-    ////////////// INDEX BUFFER
-  //Create the buffer that will hold the indices and send the data
-  var indices = [0,1,2,0,2,3,0,3,4,0,4,5,0,5,6,0,6,1];
-  var indexBuffer = gl.createBuffer();
-  //Here the buffer must be gl.ELEMENT_ARRAY_BUFFER to specify it contains indices
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-  
-
-  //drawElements uses the indices to draw the primitives
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0 );
-  //Indexed array buffer
-
-}
 
 //Call the main function once the html has finished loading
 window.onload = main;
--- shaders.js
+++ shaders.js
@@ -1,21 +1,41 @@
+
+
 var vertexShaderSource = `#version 300 es
-in vec4 a_position;
-in vec3 a_colour;
 
-out vec3 fs_colour;
+in vec3 a_position;
+in vec3 a_color;
+out vec3 colorV;
 
+uniform mat4 matrix; 
 void main() {
-  fs_colour = a_colour;
-  gl_Position = a_position;
-}`;
+  colorV = a_color;
+  gl_Position = matrix * vec4(a_position,1.0);
+}
+`;
+
+//constant color:
+// in vec3 fs_colour;
+
+
+// color using uniform vars:
+/*uniform vec3 u_colour;
+uniform float colour_choice;
+uniform vec2 u_second_colour;
+...
+vec3 fs_colour = u_colour * colour_choice + vec3(u_second_colour, 0.0) * (1.0 - colour_choice);
+*/
+
+
 
 var fragmentShaderSource = `#version 300 es
+
 precision mediump float;
 
-in vec3 fs_colour;
 
+in vec3 colorV;
 out vec4 outColor;
 
 void main() {
-  outColor = vec4(fs_colour, 1.0);
-}`;
+  outColor = vec4(colorV,1.0);
+}
+`;
